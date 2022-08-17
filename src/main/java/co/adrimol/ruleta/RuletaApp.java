c5e7f0a9421a56a3ab3466e1b4dd79af335c2c1d
admolina <adrimol@gmail.com>
2022-08-16 20:36:48 -0500

    post-get endpoint config

--- src/main/java/co/adrimol/ruleta/RuletaApp.java
+++ src/main/java/co/adrimol/ruleta/RuletaApp.java
@@ -2,9 +2,10 @@ package co.adrimol.ruleta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class RuletaApp {
+public class RuletaApp extends WebMvcConfigurerAdapter {
 
   public static void main(String[] args) {
     SpringApplication.run(RuletaApp.class, args);
--- src/main/java/co/adrimol/ruleta/controladores/RuletaController.java
+++ src/main/java/co/adrimol/ruleta/controladores/RuletaController.java
@@ -1,23 +1,33 @@
 package co.adrimol.ruleta.controladores;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class RuletaController {
 
-  @PostMapping(
-      consumes = {MediaType.APPLICATION_JSON_VALUE},
-      produces = {MediaType.APPLICATION_JSON_VALUE},
-      path = "/ruleta")
+  @GetMapping("/ruleta")
+  @ResponseBody
   public ResponseEntity getRuleta(RequestEntity<String> requestEntity) {
     try {
-      return ResponseEntity.status(HttpStatus.OK).body("<html><body>Hola</body></html>");
+      return ResponseEntity.status(HttpStatus.OK).body("<html><body>Hola Ruleta</body></html>");
+    } catch (Exception e) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+    }
+  }
+
+  @PostMapping("/ruleta-post")
+  @ResponseBody
+  public ResponseEntity getRuletaPost(RequestEntity<String> requestEntity) {
+    try {
+      return ResponseEntity.status(HttpStatus.OK)
+          .body("<html><body>Hola Ruleta Post</body></html>");
     } catch (Exception e) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
     }
