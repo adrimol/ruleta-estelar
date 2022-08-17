c76609a9e07dc3a4abf26a99799e72eec143dbeb
admolina <adrimol@gmail.com>
2022-08-16 20:42:20 -0500

    patch endpoint config

--- src/main/java/co/adrimol/ruleta/controladores/RuletaController.java
+++ src/main/java/co/adrimol/ruleta/controladores/RuletaController.java
@@ -3,31 +3,48 @@ package co.adrimol.ruleta.controladores;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class RuletaController {
 
-  @GetMapping("/ruleta")
+  @GetMapping("/iframes/game")
   @ResponseBody
   public ResponseEntity getRuleta(RequestEntity<String> requestEntity) {
     try {
-      return ResponseEntity.status(HttpStatus.OK).body("<html><body>Hola Ruleta</body></html>");
+      return ResponseEntity.status(HttpStatus.OK).body("<html><body>Ruleta Get</body></html>");
     } catch (Exception e) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
     }
   }
 
-  @PostMapping("/ruleta-post")
+  @PostMapping("/ruleta")
   @ResponseBody
   public ResponseEntity getRuletaPost(RequestEntity<String> requestEntity) {
     try {
+      return ResponseEntity.status(HttpStatus.OK).body("<html><body>Ruleta Post</body></html>");
+    } catch (Exception e) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+    }
+  }
+
+  @PatchMapping("/api/payorder/generate")
+  @ResponseBody
+  public ResponseEntity generatePayOrder(RequestEntity<String> requestEntity) {
+    try {
+      return ResponseEntity.status(HttpStatus.OK).body("<html><body>Pay Order Patch</body></html>");
+    } catch (Exception e) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+    }
+  }
+
+  @PostMapping("/api/payorder/confirm")
+  @ResponseBody
+  public ResponseEntity confirmPayOrder(RequestEntity<String> requestEntity) {
+    try {
       return ResponseEntity.status(HttpStatus.OK)
-          .body("<html><body>Hola Ruleta Post</body></html>");
+          .body("<html><body>Confirm Pay Order post</body></html>");
     } catch (Exception e) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
     }
