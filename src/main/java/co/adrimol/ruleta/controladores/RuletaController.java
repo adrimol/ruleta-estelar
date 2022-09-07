1cc724a80bd7e3f70e3e7048e73281050e4ff841
Adrian Molina <adrimol@gmail.com>
2022-09-06 21:27:33 -0500

    Agregar endpoint para test

--- src/main/java/co/adrimol/ruleta/controladores/RuletaController.java
+++ src/main/java/co/adrimol/ruleta/controladores/RuletaController.java
@@ -81,6 +81,78 @@ public class RuletaController {
     }
   }
 
+  @GetMapping("/iframes/gametest")
+  @ResponseBody
+  public ResponseEntity getRuletaTest(RequestEntity<String> requestEntity) {
+    try {
+      return ResponseEntity.status(HttpStatus.OK)
+          .body(
+              "<!DOCTYPE html>\n"
+                  + "<html>\n"
+                  + "  <head>\n"
+                  + "    <style>\n"
+                  + "      .contenedor{\n"
+                  + "      position: relative;\n"
+                  + "      display: inline-block;\n"
+                  + "      text-align: center;\n"
+                  + "      }\n"
+                  + "      .texto-encima{\n"
+                  + "      position: absolute;\n"
+                  + "      top: 10px;\n"
+                  + "      left: 10px;\n"
+                  + "      }\n"
+                  + "      .centrado{\n"
+                  + "      position: absolute;\n"
+                  + "      top: 50%;\n"
+                  + "      left: 50%;\n"
+                  + "      transform: translate(-50%, -50%);\n"
+                  + "      }\n"
+                  + "      img {\n"
+                  + "        width:100%;\n"
+                  + "        height:100%;\n"
+                  + "      }\n"
+                  + "    </style>\n"
+                  + "  </head>\n"
+                  + "  <body>\n"
+                  + "    <div class=\"contenedor\">\n"
+                  + "      <img"
+                  + " src=\"https://stellarjson.s3.us-west-2.amazonaws.com/images/roulette.png\"/>\n"
+                  + "      <div class=\"texto-encima\">\n"
+                  + "        <h3><span style=\"background-color: #914c53; color: #ffffff; padding:"
+                  + " 0 3px;\">\n"
+                  + "        Ruleta Estelar </br> 2022</span></h3>\n"
+                  + "      </div>\n"
+                  + "      <div class=\"centrado\">\n"
+                  + "        <p><strong>Digita tu número ganador y se uno de los afortunados en el"
+                  + " próximo giro</strong></p>   \n"
+                  + "        <input type=\"number\" id=\"numero\" name=\"numero\" required"
+                  + " min=\"1\" max=\"54\" >\n"
+                  + "        <button type=\"button\" id=\"btnJugar\">Jugar</button>\n"
+                  + "      </div>\n"
+                  + "    </div>\n"
+                  + "    <script>\n"
+                  + "\t\tdocument.getElementById('btnJugar').addEventListener(\"click\","
+                  + " function(e) {\n"
+                  + "\t\t\tvar message = {\n"
+                  + "        ready: true,\n"
+                  + "        keyboard_input : {\n"
+                  + "            enable : true,\n"
+                  + "            key : \"count\",\n"
+                  + "            type: \"int\"\n"
+                  + "        }\n"
+                  + "      }\n"
+                  + ";\n"
+                  + "            var domain = 'https://ruleta-estelar.herokuapp.com';\n"
+                  + "\t\t\tparent.postMessage(message,domain); //sending the message\n"
+                  + "\t\t}, false);\n"
+                  + "    </script>\n"
+                  + "  </body>\n"
+                  + "</html>");
+    } catch (Exception e) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+    }
+  }
+
   @GetMapping("/iframes/administrative")
   @ResponseBody
   public ResponseEntity getRuletaAdmin(RequestEntity<String> requestEntity) {
@@ -125,7 +197,7 @@ public class RuletaController {
 
   @GetMapping("/iframes/test")
   @ResponseBody
-  public ResponseEntity getRuletaTest(RequestEntity<String> requestEntity) {
+  public ResponseEntity getTest(RequestEntity<String> requestEntity) {
     try {
       return ResponseEntity.status(HttpStatus.OK)
           .body(
@@ -146,7 +218,7 @@ public class RuletaController {
                   + "}, false);\n"
                   + "</script>\n"
                   + "\n"
-                  + "<iframe src=\"https://ruleta-estelar.herokuapp.com/iframes/game\""
+                  + "<iframe src=\"https://ruleta-estelar.herokuapp.com/iframes/gametest\""
                   + " title=\"Ruleta Estelar\"\n"
                   + "height=500 width=500 onload=\"onRuletaLoaded(this)\">\n"
                   + "</iframe>\n"
@@ -173,10 +245,10 @@ public class RuletaController {
           .body(
               "<html><head><script type=\"text/javascript\">        function generatePayOrder() {  "
                   + "          var message = \"{\"payorder\":"
-                  + " \"ef41d3ac-2e4f-11ed-a261-0242ac120002\"\"competitors\": [{\"name\":"
+                  + " \"ef41d3ac-2e4f-11ed-a261-0242ac120002\",\"competitors\": [{\"name\":"
                   + " \"nombre\",\"id_competitor\": 2,\"category_code\": \"categoria\",\"price\":"
-                  + " 4.50,\"event_datetime\": 2022-09-15 23:50:55.999 -0500}]}\";            var"
-                  + " domain = 'https://dev.pagando.tech';           "
+                  + " 4.50,\"event_datetime\": \"2022-09-15 23:50:55.999 -0500\"}]}\";           "
+                  + " var domain = 'https://dev.pagando.tech';           "
                   + " parent.postMessage(message,domain);        }        </script></head><body"
                   + " onload=\"generatePayOrder();\">Pay Order Patch</body></html>");
     } catch (Exception e) {
