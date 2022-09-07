4e28aba188a18aaeb016234cade932c5d122728b
Adrian Molina <adrimol@gmail.com>
2022-09-06 21:14:52 -0500

    Actualizar dominio de postMessage con dev.pagando.tech

--- src/main/java/co/adrimol/ruleta/controladores/RuletaController.java
+++ src/main/java/co/adrimol/ruleta/controladores/RuletaController.java
@@ -61,8 +61,16 @@ public class RuletaController {
                   + "    <script>\n"
                   + "\t\tdocument.getElementById('btnJugar').addEventListener(\"click\","
                   + " function(e) {\n"
-                  + "\t\t\tvar message = \"A jugar Estelares!\";\n"
-                  + "            var domain = 'https://ruleta-estelar.herokuapp.com';\n"
+                  + "\t\t\tvar message = {\n"
+                  + "        ready: true,\n"
+                  + "        keyboard_input : {\n"
+                  + "            enable : true,\n"
+                  + "            key : \"count\",\n"
+                  + "            type: \"int\"\n"
+                  + "        }\n"
+                  + "      }\n"
+                  + ";\n"
+                  + "            var domain = 'https://dev.pagando.tech';\n"
                   + "\t\t\tparent.postMessage(message,domain); //sending the message\n"
                   + "\t\t}, false);\n"
                   + "    </script>\n"
@@ -76,6 +84,48 @@ public class RuletaController {
   @GetMapping("/iframes/administrative")
   @ResponseBody
   public ResponseEntity getRuletaAdmin(RequestEntity<String> requestEntity) {
+    try {
+      return ResponseEntity.status(HttpStatus.OK)
+          .body(
+              "<!DOCTYPE html>\n"
+                  + "<html>\n"
+                  + "<body>\n"
+                  + "\n"
+                  + "<div>\n"
+                  + "<h1>Ruleta parent window listener</h1>\n"
+                  + "<p>Message received: [object Object]</p>\n"
+                  + "</div>\n"
+                  + "<script>\n"
+                  + "window.addEventListener('message', function(e) {\t\n"
+                  + "  var origin = e.origin;\n"
+                  + "  document.getElementsByTagName('p')[0].innerHTML = 'Message received:  ' +"
+                  + " e.data;\n"
+                  + "  console.log('Message received:  ' + e.data);\n"
+                  + "}, false);\n"
+                  + "</script>\n"
+                  + "\n"
+                  + "<iframe src=\"https://ruleta-estelar.herokuapp.com/iframes/game\""
+                  + " title=\"Ruleta Estelar\"\n"
+                  + "height=500 width=500 onload=\"onRuletaLoaded(this)\">\n"
+                  + "</iframe>\n"
+                  + "<script>\n"
+                  + "  function onRuletaLoaded() {\n"
+                  + "    var message = 'Ruleta cargada por completo!';\n"
+                  + "    var domain = 'https://dev.pagando.tech';\n"
+                  + "\tparent.postMessage(message,domain); //sending the message\n"
+                  + "  };\n"
+                  + "  </script>\n"
+                  + "\n"
+                  + "</body>\n"
+                  + "</html>\n");
+    } catch (Exception e) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+    }
+  }
+
+  @GetMapping("/iframes/test")
+  @ResponseBody
+  public ResponseEntity getRuletaTest(RequestEntity<String> requestEntity) {
     try {
       return ResponseEntity.status(HttpStatus.OK)
           .body(
@@ -119,7 +169,16 @@ public class RuletaController {
   @ResponseBody
   public ResponseEntity generatePayOrder(RequestEntity<String> requestEntity) {
     try {
-      return ResponseEntity.status(HttpStatus.OK).body("<html><body>Pay Order Patch</body></html>");
+      return ResponseEntity.status(HttpStatus.OK)
+          .body(
+              "<html><head><script type=\"text/javascript\">        function generatePayOrder() {  "
+                  + "          var message = \"{\"payorder\":"
+                  + " \"ef41d3ac-2e4f-11ed-a261-0242ac120002\"\"competitors\": [{\"name\":"
+                  + " \"nombre\",\"id_competitor\": 2,\"category_code\": \"categoria\",\"price\":"
+                  + " 4.50,\"event_datetime\": 2022-09-15 23:50:55.999 -0500}]}\";            var"
+                  + " domain = 'https://dev.pagando.tech';           "
+                  + " parent.postMessage(message,domain);        }        </script></head><body"
+                  + " onload=\"generatePayOrder();\">Pay Order Patch</body></html>");
     } catch (Exception e) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
     }
@@ -130,7 +189,7 @@ public class RuletaController {
   public ResponseEntity confirmPayOrder(RequestEntity<String> requestEntity) {
     try {
       return ResponseEntity.status(HttpStatus.OK)
-          .body("<html><body>Confirm Pay Order post</body></html>");
+          .body("<html><body>Pay Order confirmation disabled!</body></html>");
     } catch (Exception e) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
     }
